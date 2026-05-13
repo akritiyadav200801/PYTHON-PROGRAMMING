8eda3ee77f8dbb6ea4a11485ef3a76cf520bf2b8
akritiyadav200801 <akritiyadav200801@gmail.com>
2026-05-13 09:49:20 +0530

    Add files via upload

---
+++ programs/ABCD.py
@@ -0,0 +1,7 @@
+L = int ( input ( " enter length " )
+B = int ( input ( " enter breadth " )
+A = L * B
+P = 2 * ( L + B )
+print ( " perimeter of given rectangle " , P )
+print ( " area of given rectangle ", A )
+    
